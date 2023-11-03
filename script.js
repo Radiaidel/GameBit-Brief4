92b99bc58d8f919ace2f7dc57f5c89ae315b0b9b
unknown <zinebmac49@gmail.com>
2023-11-03 12:06:15 +0100

    merging

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let cards = [];
 
 
 function fetchData() {
-  fetch('./data/products.json').then((response) => {
+  fetch('../data/products.json').then((response) => {
       return response.json();
     }).then((data) => {
       products.push(...data);
@@ -29,11 +29,12 @@ fetchData().then((result) => {
 function categories(){
   let catArray = ["All","Mouse","Keyboard","Gaming-Console","Monitor","Headset","Video-Game","Gaming-PC","Controller"];
 
-  let container = document.getElementById("cat-cards");
+  let container = document.getElementById("categories");
+  container.classList = "button d-flex justify-content-around flex-wrap  px-5";
   catArray.forEach(elem => {
     let btn = document.createElement("button");
     btn.type="button";
-    btn.classList = `btn btn-success ${elem}`;
+    btn.classList = `btn ${elem} btn btn-outline-success`;
     btn.innerHTML = elem;
 
     btn.addEventListener("click",()=>{
@@ -57,48 +58,47 @@ function displayData() {
 }
 
 function createElements() {
-  let Cards = document.createElement('div');
-  Cards.id = "cat-cards";
-  document.body.appendChild(Cards);
+  let Cards = document.getElementById("cat-cards");
 
   categories();
 
   products.forEach(elem => {
     let card = document.createElement("div");
-    card.className = `card ${elem.Category}  d-none`;
+    card.className = `card ${elem.Category} widthc flex-column justify-content-center bgc mt-5 mb-4 p-3 rounded-4 card d-none`;
 
     let img = document.createElement("img");
     img.src = elem.Images[0];
     img.alt = elem.Name;
+    img.classList = "align-self-center w-100 h-50";
 
     let title = document.createElement("h4");
     title.innerHTML = elem.Name;
-    title.classList= "card-title";
+    title.classList= "text-center text-white card-title";
 
     let description = document.createElement("p");
     description.innerHTML = elem.Description;
-    description.classList = "card-text";
+    description.classList = "text-center text-white card-text";
 
     let rate = document.createElement('div');
     rate.innerHTML = elem.Rate;
-    rate.classList = "icons";
+    rate.classList = "d-flex my-2 gap-2 align-self-center stars";
 
     let price = document.createElement('span');
     price.innerHTML = "$"+elem.Price;
-    price.classList = "price";
+    price.classList = "price text-center color1 fs-4";
 
     let btns = document.createElement('div');
-    btns.classList = "price";
+    btns.classList = "d-flex flex-row justify-content-center gap-4";
 
     let btnOutline = document.createElement('button');
     btnOutline.type = "button";
     btnOutline.innerHTML = "Personalize";
-    btnOutline.classList = "btn btn-outline-success";
+    btnOutline.classList = "btn btn-cust rounded-5";
 
     let btn = document.createElement('button');
     btn.type = "button";
     btn.innerHTML = "Add To Cart";
-    btn.classList = "btn btn-success";
+    btn.classList = "btn btn-add rounded-5";
     btn.addEventListener("click",()=>{
       addToCart(elem);
     })
@@ -115,6 +115,8 @@ function createElements() {
     btns.appendChild(btn);
 
     Cards.appendChild(card);
+
+    console.log("hhhh");
 });
 
 }
@@ -142,7 +144,10 @@ function filterByCategory(elm) {
     cards.forEach(card => {
       if (card.classList.contains(elm)) {
         card.classList.remove('d-none');
+        card.classList.add('d-flex');
       } else {
+        card.classList.remove('d-flex');
+
         card.classList.add('d-none');
       }
     });  
@@ -232,7 +237,96 @@ function addToCart() {
 });
 }
 
+function resetForm() {
+  var form = document.getElementById("contactForm");
+  form.reset();
 
+  // Réinitialisez les messages d'erreur en appelant resetErrorFeedback pour chaque champ
+  var firstName = document.getElementById("FN");
+  var lastName = document.getElementById("lname");
+  var email = document.getElementById("email");
+  var phone = document.getElementById("pnum");
+  var message = document.getElementById("message");
+
+  resetErrorFeedback(firstName);
+  resetErrorFeedback(lastName);
+  resetErrorFeedback(email);
+  resetErrorFeedback(phone);
+  resetErrorFeedback(message);
+}
+
+function showErrorFeedback(inputElement, message) {
+  inputElement.classList.add('is-invalid');
+  var invalidFeedback = inputElement.nextElementSibling;
+  invalidFeedback.textContent = message;
+}
+
+function resetErrorFeedback(inputElement) {
+  inputElement.classList.remove('is-invalid');
+  var invalidFeedback = inputElement.nextElementSibling;
+  invalidFeedback.textContent = '';
+}
+
+function validateForm(event) {
+  event.preventDefault(); // Empêche la soumission par défaut du formulaire
+  var firstName = document.getElementById("FN").value;
+  var lastName = document.getElementById("lname").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("pnum").value;
+  var message = document.getElementById("message").value;
+  // Expressions régulières pour valider les champs
+  var namePattern = /^[A-Za-z\s]{3,50}$/;
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  var phonePattern = /^\+212\d{9}$|^\d{10}$/;
+  var messagePattern = /^.{1,1000}$/;
+  // Array pour stocker les erreurs
+  var errors = [];
+  if (!namePattern.test(firstName)) {
+      showErrorFeedback(document.getElementById("FN"), "First name is invalid.");
+      errors.push("First name is invalid.");
+  } else {
+      resetErrorFeedback(document.getElementById("FN"));
+  }
+  if (!namePattern.test(lastName)) {
+      showErrorFeedback(document.getElementById("lname"), "Last name is invalid.");
+      errors.push("Last name is invalid..");
+  } else {
+      resetErrorFeedback(document.getElementById("lname"));
+  }
+  if (!emailPattern.test(email)) {
+      showErrorFeedback(document.getElementById("email"), "Please enter a valid email address.");
+      errors.push("Please enter a valid email address.");
+  } else {
+      resetErrorFeedback(document.getElementById("email"));
+  }
+
+  if (!phonePattern.test(phone)) {
+      showErrorFeedback(document.getElementById("pnum"), "Please enter a valid phone number (+212 followed by 9 digits).");
+      errors.push("Please enter a valid phone number (+212 followed by 9 digits).");
+  } else {
+      resetErrorFeedback(document.getElementById("pnum"));
+  }
+
+  if (!messagePattern.test(message)) {
+      showErrorFeedback(document.getElementById("message"), "The message must be between 1 and 1000 characters.");
+      errors.push("The message must be between 1 and 1000 characters.");
+  } else {
+      resetErrorFeedback(document.getElementById("message"));
+  }
+
+  if (errors.length > 0) {
+      return false; // Le formulaire n'est pas valide
+  }
+
+ // Le formulaire est valide,  la popup 
+ var modalContent = document.getElementById("modal-content");
+ modalContent.innerHTML = "<span class='text'>Prénom :</span> " + firstName + "&nbsp;<span class='blue-text'>Nom de famille :</span> " + lastName + "<br><span class='text'>Email :</span> " + email + "<br><span class='text'>Téléphone :</span> " + phone + "<br><span class='text'>Message :</span> " + message;
+ 
+  // Afficher la popup
+  var myModal = new bootstrap.Modal(document.getElementById('successModal'));
+  myModal.show();
+  return false;
+}
 
 
 window.addEventListener("load",()=>{
